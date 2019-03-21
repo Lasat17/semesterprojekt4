2290e0d978a224a33e31c1a7d7d659025925568d
Lasat17 <lasat17@student.sdu.dk>
2019-03-21 14:37:24 +0100

    idk

--- Unicornicopia/Player/src/main/java/dk/sdu/mmmi/cbse/player/Player.java
+++ Unicornicopia/Player/src/main/java/dk/sdu/mmmi/cbse/player/Player.java
@@ -6,6 +6,7 @@
 package dk.sdu.mmmi.cbse.player;
 
 import com.badlogic.gdx.math.MathUtils;
+import dk.sdu.mmmi.cbse.common.bullet.Bullet;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
@@ -17,104 +18,112 @@ import java.util.ArrayList;
  *
  * @author LavanSathiyaseelan
  */
-public class Player extends Entity implements IGamePluginService{
-    
-        private boolean left;
 	private boolean right;
-	private boolean up;
-        	
-	private float maxSpeed;
-	private float acceleration;
-	private float deceleration;
-	private float acceleratingTimer;
-        private float defence;
-        private int health;
-        
-        private ArrayList<Object> inventory;
-        
-        public Player() {
-                
-                defence = 1;
-                health = 100;
-            
-                x = width / 2;
-		y = height / 2;
-		
-		maxSpeed = 300;
-		acceleration = 200;
-		deceleration = 1250;
+public class Player extends Entity implements IGamePluginService {
 
-		radians = 3.1415f / 2;
-		rotationSpeed = 5;
-                
-                
-		
-	}
-        
-        public void increaseDefence(Object Item){
-            //defence = defence * (1 - Item.getDefenceValue());
+    private boolean left;
+    private boolean right;
+    private boolean up;
+
+    private final int MAX_BULLETS = 4;
+    private ArrayList<Bullet> bullets;
+
+    private float maxSpeed;
+    private float acceleration;
+    private float deceleration;
+    private float acceleratingTimer;
+    private float defence;
+    private int health;
+
+    private ArrayList<Object> inventory;
+
+    public Player(ArrayList<Bullet> bullets) {
+        this.bullets = bullets;
+        defence = 1;
+        health = 100;
+
+        x = width / 2;
+        y = height / 2;
+
+        maxSpeed = 300;
+        acceleration = 200;
+        deceleration = 1250;
+
+        radians = 3.1415f / 2;
+        rotationSpeed = 5;
+
+    }
+
+    public void increaseDefence(Object Item) {
+        //defence = defence * (1 - Item.getDefenceValue());
+    }
+
+    public void pickUpItem(Object Item) {
+        inventory.add(Item);
+    }
+
+    public void increaseHealth(int healthBoost) {
+        health = +healthBoost;
+    }
+
+    public void decreaseHealth(int damage) {
+        health = (int) (health - (damage * defence));
+    }
+
+    public void setLeft(boolean b) {
+        left = b;
+    }
+
+    public void setRight(boolean b) {
+        right = b;
+    }
+
+    public void setUp(boolean b) {
+        up = b;
+    }
+
+    public void shoot() {
+        if (bullets.size() == MAX_BULLETS) {
+            return;
         }
-        
-        public void pickUpItem(Object Item){
-            inventory.add(Item);
-        }
-        
-        public void increaseHealth(int healthBoost){
-            health =+ healthBoost;
-        }
-        
-        public void decreaseHealth(int damage){
-            health = (int) (health - (damage * defence));
-        }
-        
-        public void setLeft(boolean b) {
-            left = b; 
-        }
-        
-	public void setRight(boolean b) {
-            right = b; 
-        }
-        
-	public void setUp(boolean b) {
-            up = b; 
-        }
+        bullets.add(new Bullet(x, y, radians));
+    }
+
     public void update(float dt) {
-		
-		// turning
-		if(left) {
-			radians += rotationSpeed * dt;
-		}
-		else if(right) {
-			radians -= rotationSpeed * dt;
-		}
-		
-		// accelerating
-		if(up) {
-			dx += MathUtils.cos(radians) * acceleration * dt;
-			dy += MathUtils.sin(radians) * acceleration * dt;
-                        acceleratingTimer += dt;
-                        if (acceleratingTimer > 0.1f) {
-                            acceleratingTimer = 0;
-                    } 
-		} 
-                else{
-                    acceleratingTimer = 0;
-                }
-		
-		// deceleration
-		float vec = (float) Math.sqrt(dx * dx + dy * dy);
-		if(vec > 0 && !up) {
-			dx = 0;
-			dy = 0;
-		}
-		if(vec > maxSpeed) {
-			dx = (dx / vec) * maxSpeed;
-			dy = (dy / vec) * maxSpeed;
-		}
-		
-		// set position
-		x += dx * dt;
-		y += dy * dt;
+
+        // turning
+        if (left) {
+            radians += rotationSpeed * dt;
+        } else if (right) {
+            radians -= rotationSpeed * dt;
+        }
+
+        // accelerating
+        if (up) {
+            dx += MathUtils.cos(radians) * acceleration * dt;
+            dy += MathUtils.sin(radians) * acceleration * dt;
+            acceleratingTimer += dt;
+            if (acceleratingTimer > 0.1f) {
+                acceleratingTimer = 0;
+            }
+        } else {
+            acceleratingTimer = 0;
+        }
+
+        // deceleration
+        float vec = (float) Math.sqrt(dx * dx + dy * dy);
+        if (vec > 0 && !up) {
+            dx = 0;
+            dy = 0;
+        }
+        if (vec > maxSpeed) {
+            dx = (dx / vec) * maxSpeed;
+            dy = (dy / vec) * maxSpeed;
+        }
+
+        // set position
+        x += dx * dt;
+        y += dy * dt;
     }
 
     @Override
@@ -127,5 +136,4 @@ public class Player extends Entity implements IGamePluginService{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
 }
