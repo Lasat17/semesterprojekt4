14e1dc41f1cda678d3629075edeb8262f5d27d42
Lasat17 <lasat17@student.sdu.dk>
2019-03-21 14:36:24 +0100

    Bullet class has been modified

--- Unicornicopia/CommonBullet/src/main/java/dk/sdu/mmmi/cbse/common/bullet/Bullet.java
+++ Unicornicopia/CommonBullet/src/main/java/dk/sdu/mmmi/cbse/common/bullet/Bullet.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.cbse.common.bullet;
 
+import com.badlogic.gdx.math.MathUtils;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
 /**
@@ -7,4 +8,40 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
  * @author corfixen
  */
 public class Bullet extends Entity {
+        
+    private float lifeTime;
+    private float lifeTimer;
+    
+    private boolean remove;
+    
+    public Bullet(float x, float y, float radians) {
+        this.x = x;
+        this.y = y;
+        this.radians = radians;
+        
+        float speed = 350; 
+        dx = MathUtils.sin(radians) * speed;
+        dy = MathUtils.cos(radians) * speed;
+        
+        width = height = 2;
+        lifeTimer = 0;
+        lifeTime = 1;
+             
+             
+    }
+    
+    public boolean shouldRemove() { 
+        return remove;
+    } 
+    
+    public void update(float dt){
+        y += dx * dt;
+        x += dy * dt;
+        
+
+        lifeTimer += dt;
+        if (lifeTimer > lifeTime) {
+            remove = true;
+        }
+    }
 }
