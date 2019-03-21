3da92b7a7fd4a04c02579a6573b86f2dceb19363
Toszi <toszi18@gmail.com>
2019-03-21 10:42:08 +0100

    Found that the core was referencing OSGiCommon, which does not exsist

--- Unicornicopia/Core/src/main/java/dk/sdu/mmmi/cbse/Game.java
+++ Unicornicopia/Core/src/main/java/dk/sdu/mmmi/cbse/Game.java
@@ -109,17 +109,17 @@ public class Game implements ApplicationListener {
             sr.setColor(1, 1, 1, 1);
 
             sr.begin(ShapeRenderer.ShapeType.Line);
-
+            /*
             float[] shapex = entity.getShapeX();
             float[] shapey = entity.getShapeY();
-
+            
             for (int i = 0, j = shapex.length - 1;
                     i < shapex.length;
                     j = i++) {
 
                 sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
             }
-
+            */
             sr.end();
         }
     }
