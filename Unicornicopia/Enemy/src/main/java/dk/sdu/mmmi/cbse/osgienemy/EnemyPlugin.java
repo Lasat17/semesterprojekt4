67c719a76fd216487470dfcdf9cd64bd8253e00c
Toszi <toszi18@gmail.com>
2019-03-21 14:32:14 +0100

    Added different types of enemy and made them get individual values
    Still needs more fixing before the enemy can be properly added and the instanceof still needs to be made to get a fitting setup

--- Unicornicopia/Enemy/src/main/java/dk/sdu/mmmi/cbse/osgienemy/EnemyPlugin.java
+++ Unicornicopia/Enemy/src/main/java/dk/sdu/mmmi/cbse/osgienemy/EnemyPlugin.java
@@ -7,10 +7,14 @@ import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.enemy.Enemy;
+import dk.sdu.mmmi.cbse.common.enemy.Troll;
+import dk.sdu.mmmi.cbse.common.enemy.Wraith;
+import dk.sdu.mmmi.cbse.common.enemy.Zombie;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 public class EnemyPlugin implements IGamePluginService {
     private String enemyID;
+    String enemyType = "";
 
     public EnemyPlugin() {
     }
@@ -18,27 +22,81 @@ public class EnemyPlugin implements IGamePluginService {
     @Override
     public void start(GameData gameData, World world) {
         // Add entities to the world
-        Entity enemy = createEnemyShip(gameData);
-        enemyID = world.addEntity(enemy);
-        
+        Entity enemy = createEnemy();
+        enemyID = world.addEntity(enemy);        
     }
 
-    private Entity createEnemyShip(GameData gameData) {
-        Entity enemyShip = new Enemy();
+    private Entity createEnemy() {
+        Entity enemy = new Enemy();
+        float deacceleration;
+        float acceleration;
+        float maxSpeed;
+        float rotationSpeed;
+        float x;
+        float y;
+        float radians;
 
-        float deacceleration = 10;
-        float acceleration = 200;
-        float maxSpeed = 300;
-        float rotationSpeed = 5;
-        float x = gameData.getDisplayWidth() / 3;
-        float y = gameData.getDisplayHeight() / 3;
-        float radians = 3.1415f / 2;
-        enemyShip.add(new LifePart(3));
-        enemyShip.setRadius(4);
-        enemyShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
-        enemyShip.add(new PositionPart(x, y, radians));
+        if(enemy instanceof Troll) enemyType = "Troll";
+        if(enemy instanceof Wraith) enemyType = "Wraith";
+        if(enemy instanceof Zombie) enemyType = "Zombie";
         
-        return enemyShip;
+        switch(enemyType){
+            case "Troll": 
+                deacceleration = 500;
+                acceleration = 100;
+                maxSpeed = 100;
+                rotationSpeed = 5;
+                x = 50;
+                y = 50;
+                radians = 3.1415f / 2;
+                enemy.add(new LifePart(80));
+                enemy.setRadius(4);
+                enemy.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+                enemy.add(new PositionPart(x, y, radians));
+                break;
+            case "Wraith":
+                deacceleration = 500;
+                acceleration = 150;
+                maxSpeed = 200;
+                rotationSpeed = 5;
+                x = 50;
+                y = 50;
+                radians = 3.1415f / 2;
+                enemy.add(new LifePart(50));
+                enemy.setRadius(4);
+                enemy.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+                enemy.add(new PositionPart(x, y, radians));
+                break;
+            case "Zombie":
+                deacceleration = 500;
+                acceleration = 50;
+                maxSpeed = 175;
+                rotationSpeed = 5;
+                x = 50;
+                y = 50;
+                radians = 3.1415f / 2;
+                enemy.add(new LifePart(30));
+                enemy.setRadius(4);
+                enemy.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+                enemy.add(new PositionPart(x, y, radians));
+                break;
+            default:
+                //Boss
+                deacceleration = 500;
+                acceleration = 150;
+                maxSpeed = 200;
+                rotationSpeed = 5;
+                x = 50;
+                y = 50;
+                radians = 3.1415f / 2;
+                enemy.add(new LifePart(500));
+                enemy.setRadius(4);
+                enemy.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+                enemy.add(new PositionPart(x, y, radians));
+                break;
+        }
+        
+        return enemy;
     }
 
     @Override
